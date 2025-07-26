2d22e33f0175bf05128d1ca5e1e38509a8160df2
Philipp4253 <filippvys@gmail.com>
2025-07-26 10:47:20 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,6 +8,9 @@ from pathlib import Path
 from bot.app import BotApp
 from config.settings import Settings
 
+from aiogram.dispatcher.filters import ChatMemberUpdatedFilter
+from aiogram.dispatcher.filters.state import IS_NOT_MEMBER, IS_MEMBER
+
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
 
